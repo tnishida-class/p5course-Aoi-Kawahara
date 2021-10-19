c89915f22c4378af7dd07f4dd92ef103cfe3ecbd
Aoi-Kawahara <2000593h@gsuite.kobe-u.ac.jp>
2021-10-19 11:44:22 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,7 +1,12 @@
 // 小手調べ
 function setup(){
-  createCanvas(100, 100)
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(200, 200)
+  for(let i = 0; i < 5; i++){
+    stroke(255, 0, 0);
+    ellipse(100, 100, 100 - i * 10);
+  }
+  for(let i = 5; i < 10; i++){
+    stroke(0, 0, 255);
+    ellipse(100, 100, 100 - i * 10);
   }
 }
