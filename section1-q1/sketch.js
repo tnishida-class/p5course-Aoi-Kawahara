6fe31cbd7e3e2d45bb52bf13eb86d7065986b9b0
Aoi-Kawahara <2000593h@gsuite.kobe-u.ac.jp>
2021-10-19 11:31:57 +0900

    kobe-u logo

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -10,20 +10,35 @@ function setup(){
   strokeCap(SQUARE); //弧の端を四角形に指定
   stroke(green);
   //円の中心のx座標、y座標、円の幅、円の高さ、弧の開始角度、弧の停止角度
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(100 + 26, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
   // BLANK[1]
-  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI);
+  arc(100 - 26, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI);
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
   const blue = color(14, 47, 146);
-
   const gray = color(77);
 
   //外の青い円を描画
+  strokeCap(PROJECT);
   strokeWeight(10);
   stroke(blue);
   arc(100, 300, 100, 100, PI + HALF_PI, QUARTER_PI * 3);
+  fill(255);
+  stroke(255);
+  arc(94, 296, 94, 94, PI + HALF_PI, QUARTER_PI * 3);
   //内の緑の円を描画
   stroke(green);
-  arc(95, 300, 85, 85, PI, QUARTER_PI);
+  arc(95, 295, 85, 85, PI, QUARTER_PI);
+  fill(255);
+  stroke(255);
+  arc(93, 303, 80, 80, PI, QUARTER_PI);
+  //文字を描画
+  strokeWeight(1);
+  fill(gray);
+  stroke(gray);
+  textSize(19);
+  textFont("serif");
+  text("KOBE", 68, 290);
+  textSize(8);
+  text("UNIVERSITY", 79, 310);
 }
--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,5 +1,6 @@
 // 小手調べ
 function setup(){
+  createCanvas(100, 100)
   for(let i = 0; i < 10; i++){
     // BLANK[1]
   }
