e77371c049f07cb274ed312388e6e9cbd0baa00f
Aoi-Kawahara <2000593h@gsuite.kobe-u.ac.jp>
2021-11-29 16:50:00 +0900

    section4-2randomballs

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -22,8 +22,9 @@ function draw(){
 function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
+  let dsize = random(5, 60);
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: dsize, vx: dx, vy: dy };
     balls.push(b);
   }
 }
