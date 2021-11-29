b0763aa4557772c0db52f19563656b258c341a23
Aoi-Kawahara <2000593h@gsuite.kobe-u.ac.jp>
2021-11-29 16:36:33 +0900

    section4-q2inprogress

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,4 +1,5 @@
 // テキスト「配列を使った描画」練習問題：円グラフ
+let dx, stop;
 
 function setup(){
   createCanvas(400, 400);
@@ -7,12 +8,18 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
-
-  // BLANK[1]
+  let start = PI + HALF_PI;
+  for(let i = 0; i < scores.length; i++){
+    total += scores[i];
+    dx = scores[i] / total;
+    stop = PI + HALF_PI + (2 * PI / dx);
+    start += stop;
+    arc(width / 2, height / 2, width, height, start, stop)
+  }
 }
+//PI + HALF_PI + 2 * PI / 全体に対する割合dx
